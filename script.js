4a8e886e3af9696b9897641abafa4bf0c2547b38
Andre <ConAndre@users.noreply.github.com>
2020-12-12 18:20:55 -0500

    dynamic creation of media page

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // =============================================================================
 //  Properties for images
 // =============================================================================
-let mediaContentImg = {
+let imgData = {
   src: {
-    0: 'img/IMG_20201210_131648951.jpg',
+    0: 'img/clifford.jpg',
     1: 'img/IMG_20201210_131648951.jpg',
     2: 'img/IMG_20201210_131648951.jpg',
   },
@@ -49,81 +49,86 @@ let mediaContentVid = {
   },
 }
 
-let imagebox = [...document.getElementsByClassName("image-box")];
+function checkCards(amount) {
+  let imgCards = [...document.getElementsByClassName("card-template")];
+  console.log(imgCards.length,amount);
 
-imagebox.map(function (imgDiv, index) {
-  const cardMedia = document.createElement("div");
-  cardMedia.className = 'card mb-4 card-media';
+  console.log('Checking if cards loaded...', imgCards.length)
+  if (imgCards.length !== amount) {
+    setTimeout(checkCards, 1000, amount);
+  } else {
+    loadCards(imgCards);
+  };
+}
 
-  const cardImg = document.createElement("img");
-  Object.assign(cardImg, {className: 'card-img-top', src: mediaContentImg.src[index],})
+function loadCards(imgCards) {
+  console.log("Loading card function running...")
+  imgCards.map(function (element, index) {
+    const cardData = {
+      src: imgData.src[index],
+      title: imgData.titles[index],
+      description: imgData.descriptions[index],
+      url: imgData.url[index],
+    }
 
-  const cardBody = document.createElement("div");
-  cardBody.className = 'card-body';
+    element.getElementsByClassName('card-img-top')[0].setAttribute('src', cardData.src);
+    element.getElementsByClassName('card-title')[0].innerHTML = cardData.title;
+    element.getElementsByClassName('card-text')[0].innerHTML = cardData.description;
+    element.getElementsByClassName('card-link')[0].setAttribute('href', cardData.url);
+    element.getElementsByClassName('card-link')[0].classList.remove('d-none');
+    console.log('Mapped', index)
+    return element
+  });
+}
 
-  const cardTitle = document.createElement("h2");
-  Object.assign(cardTitle, {className: 'card-title', innerHTML: mediaContentImg.titles[index],})
-
-  const cardText = document.createElement("p");
-  Object.assign(cardText, {className: 'card-text', innerHTML: mediaContentImg.descriptions[index],})
-
-  const cardButton = document.createElement("a");
-  Object.assign(cardButton, {href: mediaContentImg.url[index], className: 'btn btn-primary', innerHTML: 'Read More &rarr;',})
-
-  cardMedia.append(cardImg, cardBody);
-  cardBody.append(cardTitle, cardText, cardButton);
-  imgDiv.append(cardMedia);
-
-  return imgDiv
-});
 
 let videobox = [...document.getElementsByClassName("video-box")];
 
-videobox.map(function (vidDiv, index) {
-  const cardMedia = document.createElement("div");
-  cardMedia.className = 'card mb-4 card-media';
+// videobox.map(function (vidDiv, index) {
+//   const cardMedia = document.createElement("div");
+//   cardMedia.className = 'card mb-4 card-media';
 
-  const cardVid = document.createElement("video");
-  Object.assign(cardVid, {
-    className: 'card-vid-top',
-    controls: true,
-    playsinline: 'playsinline',
-    muted: 'muted',
-    loop: 'loop',
-  })
+//   const cardVid = document.createElement("video");
+//   Object.assign(cardVid, {
+//     className: 'card-vid-top',
+//     controls: true,
+//     playsinline: 'playsinline',
+//     muted: 'muted',
+//     loop: 'loop',
+//   })
 
-  const cardSrc = document.createElement("source")
-  Object.assign(cardSrc, {
-    innerHTML: "Your browser does not support the video tag.",
-    src: mediaContentVid.src[index],
-    type: 'video/mp4',
-    alt: 'video',
-  })
+//   const cardSrc = document.createElement("source")
+//   Object.assign(cardSrc, {
+//     innerHTML: "Your browser does not support the video tag.",
+//     src: mediaContentVid.src[index],
+//     type: 'video/mp4',
+//     alt: 'video',
+//   })
 
-  const cardBody = document.createElement("div");
-  cardBody.className = 'card-body';
+//   const cardBody = document.createElement("div");
+//   cardBody.className = 'card-body';
 
-  const cardTitle = document.createElement("h2");
-  Object.assign(cardTitle, {className: 'card-title', innerHTML: mediaContentVid.titles[index],})
+//   const cardTitle = document.createElement("h2");
+//   Object.assign(cardTitle, {className: 'card-title', innerHTML: mediaContentVid.titles[index],})
 
-  const cardText = document.createElement("p");
-  Object.assign(cardText, {className: 'card-text', innerHTML: mediaContentVid.descriptions[index],})
+//   const cardText = document.createElement("p");
+//   Object.assign(cardText, {className: 'card-text', innerHTML: mediaContentVid.descriptions[index],})
 
-  const cardButton = document.createElement("a");
-  Object.assign(cardButton, {
-    className: 'btn btn-primary',
-    innerHTML: 'Watch Video &rarr;',
-    href: mediaContentVid.url[index],
-    target: '_blank',
-  });
+//   const cardButton = document.createElement("a");
+//   Object.assign(cardButton, {
+//     className: 'btn btn-primary',
+//     innerHTML: 'Watch Video &rarr;',
+//     href: mediaContentVid.url[index],
+//     target: '_blank',
+//   });
 
-  cardVid.append(cardSrc);
-  cardMedia.append(cardVid, cardBody);
-  cardBody.append(cardTitle, cardText, cardButton);
-  vidDiv.append(cardMedia);
+//   cardVid.append(cardSrc);
+//   cardMedia.append(cardVid, cardBody);
+//   cardBody.append(cardTitle, cardText, cardButton);
+//   vidDiv.append(cardMedia);
 
-  return vidDiv
-});
+//   return vidDiv
+// });
 
 
 

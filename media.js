4007ef11620fe8b089ec4e19136d498c1a049b29
Andre <ConAndre@users.noreply.github.com>
2020-12-12 20:27:43 -0500

    html injection after html loads

--- media.js
+++ media.js
@@ -5,32 +5,32 @@ mediaNameSpace = function() {
   function initMediaData() {
     return {
       src: {
-        "0": "img/clifford.jpg",
-        "1": "img/IMG_20201210_131648951.jpg",
-        "2": "img/IMG_20201210_131648951.jpg",
-        "3": "img/COS_preview.mp4",
-        "4": "img/SS_preview.mp4"
+        0: "img/clifford.jpg",
+        1: "img/IMG_20201210_131648951.jpg",
+        2: "img/IMG_20201210_131648951.jpg",
+        3: "img/COS_preview.mp4",
+        4: "img/SS_preview.mp4"
       },
       titles: {
-        "0": "Post Title",
-        "1": "Hello World",
-        "2": "Hello World",
-        "3": "Hello World",
-        "4": "Hello World"
+        0: "Post Title",
+        1: "Hello World",
+        2: "Hello World",
+        3: "Hello World",
+        4: "Hello World"
       },
       descriptions: {
-        "0": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
-        "1": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
-        "2": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
-        "3": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
-        "4": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!"
+        0: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!"
       },
       url: {
-        "0": "media2.html",
-        "1": "media2.html",
-        "2": "media2.html",
-        "3": "media2.html",
-        "4": "media2.html"
+        0: "media2.html",
+        1: "media2.html",
+        2: "media2.html",
+        3: "media2.html",
+        4: "media2.html"
       }
     }
   }

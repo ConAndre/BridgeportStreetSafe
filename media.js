04be5e35e4350d2a2f66a24b4f8c6db56ff8dea0
Andre <ConAndre@users.noreply.github.com>
2020-12-12 19:41:13 -0500

    optimization, linting

--- media.js
+++ media.js
@@ -1,47 +1,48 @@
 // =============================================================================
-//  Method for Images
+//  Method for Media
 // =============================================================================
-
 mediaNameSpace = function() {
   function initMediaData() {
     return {
       src: {
-        0: 'img/clifford.jpg',
-        1: 'img/IMG_20201210_131648951.jpg',
-        2: 'img/IMG_20201210_131648951.jpg',
-        3: 'img/COS_preview.mp4',
-        4: 'img/SS_preview.mp4',
+        "0": "img/clifford.jpg",
+        "1": "img/IMG_20201210_131648951.jpg",
+        "2": "img/IMG_20201210_131648951.jpg",
+        "3": "img/COS_preview.mp4",
+        "4": "img/SS_preview.mp4"
       },
       titles: {
-        0: 'Post Title',
-        1: 'Hello World',
-        2: 'Hello World',
-        3: 'Hello World',
-        4: 'Hello World',
+        "0": "Post Title",
+        "1": "Hello World",
+        "2": "Hello World",
+        "3": "Hello World",
+        "4": "Hello World"
       },
       descriptions: {
-        0: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
-        1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
-        2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
-        3: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
-        4: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+        "0": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        "1": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        "2": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        "3": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
+        "4": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!"
       },
       url: {
-        0: 'media2.html',
-        1: 'media2.html',
-        2: 'media2.html',
-        3: 'media2.html',
-        4: 'media2.html',
-      },
+        "0": "media2.html",
+        "1": "media2.html",
+        "2": "media2.html",
+        "3": "media2.html",
+        "4": "media2.html"
+      }
     }
   }
   function checkCards(imgCount, vidCount) {
     const count = imgCount + vidCount;
-    let cards = [...document.getElementsByClassName("card-template")];
+    let cards = Object.assign({}, document.getElementsByClassName("card-template"));
 
-    if (cards.length !== count) {
-      setTimeout(checkCards, 1000, imgCount, vidCount);
+    if (Object.keys(cards).length !== count) {
+      setTimeout(checkCards, 650, imgCount, vidCount);
     } else {
+      // With more values, the conversion to array takes longer.
+      cards = Object.values(cards);
       loadCards(cards);
     };
   }
@@ -55,11 +56,11 @@ mediaNameSpace = function() {
         url: mediaData.url[index],
       }
 
-      element.getElementsByClassName('card-img-top')[0].setAttribute('src', cardData.src);
-      element.getElementsByClassName('card-title')[0].innerHTML = cardData.title;
-      element.getElementsByClassName('card-text')[0].innerHTML = cardData.description;
-      element.getElementsByClassName('card-link')[0].setAttribute('href', cardData.url);
-      element.getElementsByClassName('card-link')[0].classList.remove('d-none');
+      element.getElementsByClassName("card-img-top")[0].setAttribute("src", cardData.src);
+      element.getElementsByClassName("card-title")[0].innerHTML = cardData.title;
+      element.getElementsByClassName("card-text")[0].innerHTML = cardData.description;
+      element.getElementsByClassName("card-link")[0].setAttribute("href", cardData.url);
+      element.getElementsByClassName("card-link")[0].classList.remove("d-none");
       return element
     });
   }

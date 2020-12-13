64e4c897471a6ff07f271f897a7ee47bcb875bb9
Andre <ConAndre@users.noreply.github.com>
2020-12-12 23:49:26 -0500

    added dynamic sidepanel to link to posts

--- media.js
+++ media.js
@@ -12,11 +12,11 @@ mediaNameSpace = function() {
         4: "img/SS_preview.mp4"
       },
       titles: {
-        0: "Post Title",
-        1: "Hello World",
-        2: "Hello World",
-        3: "Hello World",
-        4: "Hello World"
+        0: "Clifford",
+        1: "Toy Donation",
+        2: "Toy Donation 2",
+        3: "City of Shooters",
+        4: "StreetSafe Program"
       },
       descriptions: {
         0: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!",
@@ -34,14 +34,27 @@ mediaNameSpace = function() {
       }
     }
   }
+  function loadSidePanel(mediaData) {
+    let sidePanel = document.getElementsByClassName("media-sidepanel-list")[0];
+    let buttons = Object.values(mediaData.titles);
+    buttons.forEach(title => {
+      const listItem = document.createElement("li")
+      let anchor = document.createElement("a")
+      anchor.innerHTML = title;
+      title = "#"+title.replace(/\s+/g, '-').toLowerCase();
+      anchor.href = title;
+      listItem.appendChild(anchor);
+      sidePanel.appendChild(listItem);
+    });
+  }
+
   function checkCards(imgCount, vidCount) {
     const count = imgCount + vidCount;
-    let cards = Object.assign({}, document.getElementsByClassName("card-template"));
-    if (Object.keys(cards).length !== count) {
+    let cards = Object.assign([], document.getElementsByClassName("media-card-template"));
+    if (cards.length !== count) {
       setTimeout(checkCards, 650, imgCount, vidCount);
     } else {
       // With more values, the conversion to array takes longer.
-      cards = Object.values(cards);
       loadCards(cards);
     };
   }
@@ -54,14 +67,16 @@ mediaNameSpace = function() {
         description: mediaData.descriptions[index],
         url: mediaData.url[index],
       }
-
+      if (!!!cardData.src || !!!cardData.title || !!!cardData.description || !!!cardData.url) return alert(`Missing post information about #${index}`)
       element.getElementsByClassName("card-img-top")[0].setAttribute("src", cardData.src);
+      element.getElementsByClassName("card-img-top")[0].id = cardData.title.replace(/\s+/g, '-').toLowerCase();
       element.getElementsByClassName("card-title")[0].innerHTML = cardData.title;
       element.getElementsByClassName("card-text")[0].innerHTML = cardData.description;
       element.getElementsByClassName("card-link")[0].setAttribute("href", cardData.url);
       element.getElementsByClassName("card-link")[0].classList.remove("d-none");
       return element
     });
+    loadSidePanel(mediaData);
   }
   return {
     checkCards:checkCards
